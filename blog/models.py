44e64a8162109ef250ee35af60390fe8dd3dca44
hannan <hannan@macbook-pro.home>
2021-03-11 19:47:38 +0100

    added mdbootstrap

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,24 @@
 from django.db import models
+from django.shortcuts import reverse
 
 # Create your models here.
-def Post(models.Model):
+
+class Category(models.Model):
+	title = models.CharField(max_length=50)
+
+	def __str__(self):
+		return self.title
+
+	class Meta:
+		verbose_name_plural = "Categories"
+
+class Post(models.Model):
 	title = models.CharField(max_length=50)
 	content = models.TextField(max_length=255)
-	
+	category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+
+	def __str__(self):
+		return self.title
+
+	def get_absolute_url(self):
+		return reverse("post_detail", args=[str(self.pk)])
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView
+from .models import Post
 # Create your views here.
+
+class PostListView(ListView):
+	model = Post
+	queryset = Post.objects.all()
+	context_object_name = "posts"
+	template_name = "blog/post_list.html"
+
+
+class PostDetailView(DetailView):
+	model = Post
+	template_name = "blog/post_detail.html"
+	context_object_name = "post"
